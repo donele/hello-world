6cb704ff76504672ec18e3a4dd0571ad36421d0d
Jedong Lee <dxdy@yahoo.com>
2024-06-14 13:09:56 -0400

    Loop two input files

--- cpp/job_chain/binsrc/run_chain.cpp
+++ cpp/job_chain/binsrc/run_chain.cpp
@@ -1,22 +1,23 @@
 #include <job_chain.h>
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 int main() {
-    const string filename = "input_few.txt";
-    cout << "Few chains" << endl;
+    vector<string> filenames = {"input_few.txt", "input_many.txt"};
+    for(string& filename : filenames) {
+        cout << filename << endl;
 
-    cout << "Using vector..." << endl;
-	process_jobs_vector(filename, true);
+        cout << "Using vector..." << endl;
+	    process_jobs_vector(filename, true);
 
-    cout << "Using map..." << endl;
-	process_jobs_map(filename, true);
+        cout << "Using map..." << endl;
+	    process_jobs_map(filename, true);
 
-    cout << "Using unordered map..." << endl;
-	process_jobs_unordered_map(filename, true);
-
-    cout << "Using unordered map::extract..." << endl;
-	process_jobs_unordered_map_extract(filename, true);
+        cout << "Using unordered map..." << endl;
+	    process_jobs_unordered_map(filename, true);
+    }
 
 	return 0;
 }
