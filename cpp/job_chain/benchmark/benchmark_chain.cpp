f12a472872d51f61a1b845f05db3271eeb22c890
Jedong Lee <jdlee@linux-m715q-1.lan>
2024-06-14 08:49:58 -0400

    Make names shorter

--- cpp/job_chain/benchmark/benchmark_chain.cpp
+++ cpp/job_chain/benchmark/benchmark_chain.cpp
@@ -5,48 +5,69 @@ using namespace std;
 
 const string file1 = "input_few.txt";
 
-static void bm_job_chain_map_few(benchmark::State& state) {
+static void map_few(benchmark::State& state) {
     for(auto _ : state) {
         process_jobs_map(file1);
     }
 }
-BENCHMARK(bm_job_chain_map_few);
+BENCHMARK(map_few);
 
-static void bm_job_chain_unordered_map_few(benchmark::State& state) {
+
+static void unordered_map_few(benchmark::State& state) {
     for(auto _ : state) {
         process_jobs_unordered_map(file1);
     }
 }
-BENCHMARK(bm_job_chain_unordered_map_few);
+BENCHMARK(unordered_map_few);
 
-static void bm_job_chain_vector_few(benchmark::State& state) {
+
+static void unordered_map_extract_few(benchmark::State& state) {
+    for(auto _ : state) {
+        process_jobs_unordered_map_extract(file1);
+    }
+}
+BENCHMARK(unordered_map_extract_few);
+
+
+static void vector_few(benchmark::State& state) {
     for(auto _ : state) {
         process_jobs_vector(file1);
     }
 }
-BENCHMARK(bm_job_chain_vector_few);
+BENCHMARK(vector_few);
+
 
 const string file2 = "input_many.txt";
 
-static void bm_job_chain_map_many(benchmark::State& state) {
+static void map_many(benchmark::State& state) {
     for(auto _ : state) {
         process_jobs_map(file2);
     }
 }
-BENCHMARK(bm_job_chain_map_many);
+BENCHMARK(map_many);
 
-static void bm_job_chain_unordered_map_many(benchmark::State& state) {
+
+static void unordered_map_many(benchmark::State& state) {
     for(auto _ : state) {
         process_jobs_unordered_map(file2);
     }
 }
-BENCHMARK(bm_job_chain_unordered_map_many);
+BENCHMARK(unordered_map_many);
 
-static void bm_job_chain_vector_many(benchmark::State& state) {
+
+static void unordered_map_extract_many(benchmark::State& state) {
+    for(auto _ : state) {
+        process_jobs_unordered_map_extract(file2);
+    }
+}
+BENCHMARK(unordered_map_extract_many);
+
+
+static void vector_many(benchmark::State& state) {
     for(auto _ : state) {
         process_jobs_vector(file2);
     }
 }
-BENCHMARK(bm_job_chain_vector_many);
+BENCHMARK(vector_many);
 
 BENCHMARK_MAIN();
