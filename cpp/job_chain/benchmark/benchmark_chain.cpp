f02a6073c4d02212631644adb76a8b84a4220129
Jedong Lee <dxdy@yahoo.com>
2024-06-14 18:18:45 -0400

    Set iteration to 2

--- cpp/job_chain/benchmark/benchmark_chain.cpp
+++ cpp/job_chain/benchmark/benchmark_chain.cpp
@@ -10,7 +10,7 @@ static void BM_vector_few(benchmark::State& state) {
         process_jobs_vector(file1);
     }
 }
-BENCHMARK(BM_vector_few);
+BENCHMARK(BM_vector_few)->Iterations(2);
 
 
 static void BM_map_few(benchmark::State& state) {
@@ -18,7 +18,7 @@ static void BM_map_few(benchmark::State& state) {
         process_jobs_map(file1);
     }
 }
-BENCHMARK(BM_map_few);
+BENCHMARK(BM_map_few)->Iterations(2);
 
 
 static void BM_unordered_map_few(benchmark::State& state) {
@@ -26,7 +26,7 @@ static void BM_unordered_map_few(benchmark::State& state) {
         process_jobs_unordered_map(file1);
     }
 }
-BENCHMARK(BM_unordered_map_few);
+BENCHMARK(BM_unordered_map_few)->Iterations(2);
 
 
 const string file2 = "input_many.txt";
@@ -36,7 +36,7 @@ static void BM_vector_many(benchmark::State& state) {
         process_jobs_vector(file2);
     }
 }
-BENCHMARK(BM_vector_many);
+BENCHMARK(BM_vector_many)->Iterations(1);
 
 
 static void BM_map_many(benchmark::State& state) {
@@ -44,7 +44,7 @@ static void BM_map_many(benchmark::State& state) {
         process_jobs_map(file2);
     }
 }
-BENCHMARK(BM_map_many);
+BENCHMARK(BM_map_many)->Iterations(1);
 
 
 static void BM_unordered_map_many(benchmark::State& state) {
@@ -52,7 +52,7 @@ static void BM_unordered_map_many(benchmark::State& state) {
         process_jobs_unordered_map(file2);
     }
 }
-BENCHMARK(BM_unordered_map_many);
+BENCHMARK(BM_unordered_map_many)->Iterations(1);
 
 
 BENCHMARK_MAIN();
