b15701adeea2f3ee346ed586aa6e161bb1b383d2
Jedong Lee <dxdy@yahoo.com>
2024-06-13 23:10:11 -0400

    Benchmark for different number of chains

--- cpp/job_chain/benchmark/benchmark_chain.cpp
+++ cpp/job_chain/benchmark/benchmark_chain.cpp
@@ -1,18 +1,52 @@
+#include <string>
 #include <benchmark/benchmark.h>
 #include <job_chain.h>
+using namespace std;
 
-static void bm_job_chain_map(benchmark::State& state) {
+const string file1 = "input_few.txt";
+
+static void bm_job_chain_map_few(benchmark::State& state) {
     for(auto _ : state) {
-        process_jobs_map();
+        process_jobs_map(file1);
     }
 }
-BENCHMARK(bm_job_chain_map);
+BENCHMARK(bm_job_chain_map_few);
 
-static void bm_job_chain_vector(benchmark::State& state) {
+static void bm_job_chain_unordered_map_few(benchmark::State& state) {
     for(auto _ : state) {
-        process_jobs_vector();
+        process_jobs_unordered_map(file1);
     }
 }
-BENCHMARK(bm_job_chain_vector);
+BENCHMARK(bm_job_chain_unordered_map_few);
+
+static void bm_job_chain_vector_few(benchmark::State& state) {
+    for(auto _ : state) {
+        process_jobs_vector(file1);
+    }
+}
+BENCHMARK(bm_job_chain_vector_few);
+
+const string file2 = "input_many.txt";
+
+static void bm_job_chain_map_many(benchmark::State& state) {
+    for(auto _ : state) {
+        process_jobs_map(file2);
+    }
+}
+BENCHMARK(bm_job_chain_map_many);
+
+static void bm_job_chain_unordered_map_many(benchmark::State& state) {
+    for(auto _ : state) {
+        process_jobs_unordered_map(file2);
+    }
+}
+BENCHMARK(bm_job_chain_unordered_map_many);
+
+static void bm_job_chain_vector_many(benchmark::State& state) {
+    for(auto _ : state) {
+        process_jobs_vector(file2);
+    }
+}
+BENCHMARK(bm_job_chain_vector_many);
 
 BENCHMARK_MAIN();
