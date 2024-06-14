d3fa60267848942907179e049f5ee0010c99ad5b
Jedong Lee <dxdy@yahoo.com>
2024-06-14 13:10:16 -0400

    Add Job struct

--- cpp/job_chain/include/job_chain.h
+++ cpp/job_chain/include/job_chain.h
@@ -2,9 +2,20 @@
 #define __job_chain_h__
 #include<string>
 
-bool process_jobs_unordered_map_extract(const std::string& filename, bool verbose=false);
-bool process_jobs_unordered_map(const std::string& filename, bool verbose=false);
-bool process_jobs_map(const std::string& filename, bool verbose=false);
+struct Job {
+    int id;
+    int runtime;
+    int next;
+    bool valid;
+    Job(int _id, int _runtime, int _next, bool v):id(_id), runtime(_runtime), next(_next), valid(v){}
+};
+
+std::vector<std::string> split_comma(const std::string& s);
+bool valid(const std::vector<std::string>& v);
+Job parse_line(const std::string& s);
+
 bool process_jobs_vector(const std::string& filename, bool verbose=false);
+bool process_jobs_map(const std::string& filename, bool verbose=false);
+bool process_jobs_unordered_map(const std::string& filename, bool verbose=false);
 
 #endif
