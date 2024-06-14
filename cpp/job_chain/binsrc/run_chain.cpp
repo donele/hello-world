3c8c4bb9891e020a03879f7d52004592a1c14605
Jedong Lee <dxdy@yahoo.com>
2024-06-13 23:10:38 -0400

    Add unordered_map case

--- cpp/job_chain/binsrc/run_chain.cpp
+++ cpp/job_chain/binsrc/run_chain.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 int main() {
+    cout << "Few chains" << endl;
+    cout << "Using unordered map..." << endl;
+	process_jobs_unordered_map("input_few.txt", true);
     cout << "Using map..." << endl;
-	process_jobs_map(true);
+	process_jobs_map("input_few.txt", true);
     cout << "Using vector..." << endl;
-	process_jobs_vector(true);
+	process_jobs_vector("input_few.txt", true);
 	return 0;
 }
--- cpp/job_chain/include/job_chain.h
+++ cpp/job_chain/include/job_chain.h
@@ -1,7 +1,9 @@
 #ifndef __job_chain_h__
 #define __job_chain_h__
+#include<string>
 
-bool process_jobs_map(bool verbose=false);
-bool process_jobs_vector(bool verbose=false);
+bool process_jobs_map(const std::string& filename, bool verbose=false);
+bool process_jobs_unordered_map(const std::string& filename, bool verbose=false);
+bool process_jobs_vector(const std::string& filename, bool verbose=false);
 
 #endif
