19d8574fccd73b83fbb507c3e5e0021803bb7ec2
Jedong Lee <dxdy@yahoo.com>
2024-06-14 13:10:39 -0400

    Add parse_line

--- cpp/job_chain/src/chain.cpp
+++ cpp/job_chain/src/chain.cpp
@@ -8,19 +8,19 @@
 using namespace std;
 
 struct Chain {
-	int cnt;
-	int sum;
-	vector<int> ids;
-	Chain():cnt(0), sum(0){}
-	void add(int id, int runtime) {
-		++cnt;
-		ids.push_back(id);
-		sum += runtime;
-	}
-	void report() {
-		cout << "First id: " << ids[0] << endl;
-		cout << "Last id: " << ids[cnt - 1] << endl;
-	}
+    int cnt;
+    int sum;
+    vector<int> ids;
+    Chain():cnt(0), sum(0){}
+    void add(int id, int runtime) {
+        ++cnt;
+        ids.push_back(id);
+        sum += runtime;
+    }
+    void report() {
+        cout << "First id: " << ids[0] << endl;
+        cout << "Last id: " << ids[cnt - 1] << endl;
+    }
 };
 
 vector<string> split_comma(const string& s) {
@@ -59,166 +59,180 @@ bool valid(const vector<string>& v) {
 	return valid_size && non_empty && all_digits;
 }
 
-void throw_error() {
-	cout << "Malformed";
+Job parse_line(const string& s) {
+    int n = s.size();
+    int i = 0;
+    int j = 0;
+    int x = 0;
+    while(j < n && isdigit(s[j])) {
+        ++j;
+    }
+    if(j > i) {
+        for(int k = i; k < j; ++k) {
+            x *= 10;
+            x += s[k] - '0';
+        }
+    } else {
+        return Job(0, 0, 0, false);
+    }
+    if(s[j] == ',' && isdigit(s[j+1])) {
+        i = j + 1;
+        j = i;
+    } else {
+        return Job(0, 0, 0, false);
+    }
+
+    int y = 0;
+    while(j < n && isdigit(s[j])) {
+        ++j;
+    }
+    if(j > i) {
+        for(int k = i; k < j; ++k) {
+            y *= 10;
+            y += s[k] - '0';
+        }
+    } else {
+        return Job(0, 0, 0, false);
+    }
+    if(s[j] == ',' && isdigit(s[j+1])) {
+        i = j + 1;
+        j = i;
+    } else {
+        return Job(0, 0, 0, false);
+    }
+
+    int z = 0;
+    while(j < n && isdigit(s[j])) {
+        ++j;
+    }
+    if(j > i && j == n) {
+        for(int k = i; k < j; ++k) {
+            z *= 10;
+            z += s[k] - '0';
+        }
+    } else {
+        return Job(0, 0, 0, false);
+    }
+
+    return Job(x, y, z, true);
 }
 
-bool process_jobs_unordered_map_extract(const string& filename, bool verbose) {
-	unordered_map<int, Chain> chains;
-	vector<Chain> done;
-
-	ifstream ifs(filename);
-	string line;
-	getline(ifs, line);
-
-	for(; getline(ifs, line);) {
-		vector<string> split = split_comma(line);
-		if(valid(split)) {
-			int id = stoi(split[0]);
-			int runtime = stoi(split[1]);
-			int next = stoi(split[2]);
-			if(chains.count(id)) {
-                if(next == 0) {
-                    done.push_back(chains[id]);
-                    chains.erase(id);
-                } else {
-                    auto nh = chains.extract(id);
-                    nh.key() = next;
-                    chains.insert(move(nh));
-                }
-			} else {
-				chains[next].add(id, runtime);
-			}
-		} else {
-			throw_error();
-			return false;
-		}
-	}
-    if(verbose) {
-	    for(auto& ch : done) {
-		    cout << "-" << endl;
-		    ch.report();
-	    }
-	    cout << "-" << endl;
-    }
-	return true;
+void throw_error() {
+    cout << "Malformed";
 }
 
 bool process_jobs_unordered_map(const string& filename, bool verbose) {
-	unordered_map<int, Chain> chains;
-	vector<Chain> done;
+    unordered_map<int, Chain> chains;
+    ifstream ifs(filename);
+    string line;
+    getline(ifs, line);
 
-	ifstream ifs(filename);
-	string line;
-	getline(ifs, line);
-
-	for(; getline(ifs, line);) {
-		vector<string> split = split_comma(line);
-		if(valid(split)) {
-			int id = stoi(split[0]);
-			int runtime = stoi(split[1]);
-			int next = stoi(split[2]);
-			if(chains.count(id)) {
-				chains[id].add(id, runtime);
-				if(next == 0)
-					done.push_back(chains[id]);
-				else
-					chains[next] = move(chains[id]);
-				chains.erase(id);
-			} else {
-				chains[next].add(id, runtime);
-			}
-		} else {
-			throw_error();
-			return false;
-		}
-	}
-    if(verbose) {
-	    for(auto& ch : done) {
-		    cout << "-" << endl;
-		    ch.report();
-	    }
-	    cout << "-" << endl;
+    for(; getline(ifs, line);) {
+        Job job = parse_line(line);
+        if(job.valid) {
+            auto it = chains.find(job.id);
+            if(it != chains.end()) {
+                it->second.add(job.id, job.runtime);
+                if(job.next == 0) {
+                    auto nh = chains.extract(it);
+                    nh.key() = it->second.ids[0];
+                    chains.insert(move(nh));
+                } else {
+                    auto nh = chains.extract(it);
+                    nh.key() = job.next;
+                    chains.insert(move(nh));
+                }
+            } else {
+                auto ret = chains.insert(make_pair(job.next, Chain()));
+                ret.first->second.add(job.id, job.runtime);
+            }
+        } else {
+            throw_error();
+            return false;
+        }
     }
-	return true;
+    if(verbose) {
+        for(auto& ch : chains) {
+            cout << "-" << endl;
+            ch.second.report();
+        }
+        cout << "-" << endl;
+    }
+    return true;
 }
 
 bool process_jobs_map(const string& filename, bool verbose) {
-	map<int, Chain> chains;
-	vector<Chain> done;
+    map<int, Chain> chains;
+    ifstream ifs(filename);
+    string line;
+    getline(ifs, line);
 
-	ifstream ifs(filename);
-	string line;
-	getline(ifs, line);
-
-	for(; getline(ifs, line);) {
-		vector<string> split = split_comma(line);
-		if(valid(split)) {
-			int id = stoi(split[0]);
-			int runtime = stoi(split[1]);
-			int next = stoi(split[2]);
-			if(chains.count(id)) {
-				chains[id].add(id, runtime);
-				if(next == 0)
-					done.push_back(chains[id]);
-				else
-					chains[next] = move(chains[id]);
-				chains.erase(id);
-			} else {
-				chains[next].add(id, runtime);
-			}
-		} else {
-			throw_error();
-			return false;
-		}
-	}
-    if(verbose) {
-	    for(auto& ch : done) {
-		    cout << "-" << endl;
-		    ch.report();
-	    }
-	    cout << "-" << endl;
+    for(; getline(ifs, line);) {
+        Job job = parse_line(line);
+        if(job.valid) {
+            auto it = chains.find(job.id);
+            if(it != chains.end()) {
+                it->second.add(job.id, job.runtime);
+                if(job.next == 0) {
+                    auto nh = chains.extract(it);
+                    nh.key() = it->second.ids[0];
+                    chains.insert(move(nh));
+                } else {
+                    auto nh = chains.extract(it);
+                    nh.key() = job.next;
+                    chains.insert(move(nh));
+                }
+            } else {
+                auto ret = chains.insert(make_pair(job.next, Chain()));
+                ret.first->second.add(job.id, job.runtime);
+            }
+        } else {
+            throw_error();
+            return false;
+        }
     }
-	return true;
+    if(verbose) {
+        for(auto& ch : chains) {
+            cout << "-" << endl;
+            ch.second.report();
+        }
+        cout << "-" << endl;
+    }
+    return true;
 }
 
 bool process_jobs_vector(const string& filename, bool verbose) {
-    vector<int> next_id;
     vector<Chain> chains;
+    vector<int> next_id;
+    ifstream ifs(filename);
+    string line;
+    getline(ifs, line);
 
-	ifstream ifs(filename);
-	string line;
-	getline(ifs, line);
-
-	for(; getline(ifs, line);) {
-		vector<string> split = split_comma(line);
-		if(valid(split)) {
-			int id = stoi(split[0]);
-			int runtime = stoi(split[1]);
-			int next = stoi(split[2]);
-
-            auto it = find(next_id.begin(), next_id.end(), id);
-			if(it == next_id.end()) {
+    for(; getline(ifs, line);) {
+        Job job = parse_line(line);
+        if(job.valid) {
+            auto it = find(next_id.begin(), next_id.end(), job.id);
+            if(it == next_id.end()) {
                 Chain ch;
+                ch.add(job.id, job.runtime);
                 chains.push_back(ch);
-                chains[chains.size() - 1].add(id, runtime);
-                next_id.push_back(next);
+                next_id.push_back(job.next);
             } else {
                 int ichain = distance(next_id.begin(), it);
-                chains[ichain].add(id, runtime);
-                next_id[ichain] = next;
+                chains[ichain].add(job.id, job.runtime);
+                next_id[ichain] = job.next;
             }
-		} else {
-			throw_error();
-			return false;
-		}
-	}
-    if(verbose) {
-	    for(auto& ch : chains) {
-		    cout << "-" << endl;
-		    ch.report();
-	    }
-	    cout << "-" << endl;
+        } else {
+            throw_error();
+            return false;
+        }
     }
-	return true;
+    if(verbose) {
+        for(auto& ch : chains) {
+            cout << "-" << endl;
+            ch.report();
+        }
+        cout << "-" << endl;
+    }
+    return true;
 }
